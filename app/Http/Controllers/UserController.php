a904c2fda59751eacc399ea00d1e38a3cb7d064d
Nslkh <olluzran@gmail.com>
2021-05-10 16:13:45 +0500

    update user details

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -66,7 +66,8 @@ class UserController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = (new User)->findUser($id);
+        return view('backend.user.edit',compact('user'));
     }
 
     /**
@@ -78,7 +79,11 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request,[
+            'name'=>'required'
+        ]);
+        $user = (new User)->updateUser($request->all(),$id);
+        return redirect()->route('user.index')->with('message','User Updated successfully');
     }
 
     /**
