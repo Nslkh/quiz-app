5acc81104c70584fc445634f91d7ad8289c89d5d
Nslkh <olluzran@gmail.com>
2021-05-11 23:41:34 +0500

    (Frontend) getting one question on screen using vue js

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -1,14 +1,15 @@
+
 @extends('layouts.app')
 
 @section('content')
 
-<quiz-component>
+<quiz-component
+:times = "{{ $quiz->minutes }}"
+:quizId = "{{ $quiz->id }}"
+:quiz-questions = "{{ $quizQuestions }}"
+:has-quiz-played = "{{ $authUserHasPlayedQuiz }}"
+>
+	
+</quiz-component>
 
-  :times ="{{$quiz->minutes }}"
-  :quizId ="{{$quiz->id }}"
-  :quiz-questions ="{{$quizQuestions}}"
-  :has-quiz-played ="{{$authUserHasPlayedQuiz }}"
-  >
-
-</quiz-component>  
 @endsection
